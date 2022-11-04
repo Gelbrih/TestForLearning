7653a13e83a47588a05ee6b0b6e7acae2084ab91
Gelbrih <dreamlikeforest@gmail.com>
2022-11-04 14:00:28 +0300

    delete anclean+alerts

--- src/js/main.js
+++ src/js/main.js
@@ -8,23 +8,6 @@ let arr = [5, 3, 8, 1];
 
 let filtered = filterRange(arr, 1, 4);
 
-alert(filtered); // 3,1 (совпадающие значения)
-
-alert(arr); // 5,3,8,1 (без изменений)
-
-
-function aclean(arr) {
-   let map = new Map();
-
-   for (let word of arr) {
-      // разбиваем слово на буквы, сортируем и объединяем снова в строку
-      let sorted = word.toLowerCase().split("").sort().join(""); // (*)
-      map.set(sorted, word);
-   }
-
-   return Array.from(map.values());
-}
-
 let messages = [
    { text: "Hello", from: "John" },
    { text: "How goes?", from: "John" },
