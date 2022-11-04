2af0a2eb7ecdc0f107ebbe6fa2787945d5c8892d
Gelbrih <dreamlikeforest@gmail.com>
2022-11-04 13:27:30 +0300

    add filter function

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,15 @@
 'use strict'
-let fruits = ["Яблоки", "Груша", "Апельсин"];
-
-let shoppingCart = fruits;
-
-shoppingCart.push("Банан");
-
-alert(fruits.length); // 4
-
-
-function getMaxSubSum(arr) {
-   let maxSum = 0;
-   let partialSum = 0;
-
-   for (let item of arr) { // для каждого элемента массива
-      partialSum += item; // добавляем значение элемента к partialSum
-      maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
-      if (partialSum < 0) partialSum = 0; // ноль если отрицательное
-   }
-
-   return maxSum;
+function filterRange(arr, a, b) {
+   // добавлены скобки вокруг выражения для улучшения читабельности
+   return arr.filter(item => (a <= item && item <= b));
 }
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert(filtered); // 3,1 (совпадающие значения)
+
+alert(arr); // 5,3,8,1 (без изменений)
 //import { slider } from "./modules/_slider.js";
 //import { modal} from "./modules/modal.js";
