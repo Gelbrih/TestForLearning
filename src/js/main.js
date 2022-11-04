5ff196fca3b11866ccdb1163fe681b795f7f4b10
Gelbrih <dreamlikeforest@gmail.com>
2022-11-04 13:19:05 +0300

    add random task

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 'use strict'
-console.log(1);
+let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+let shoppingCart = fruits;
+
+shoppingCart.push("Банан");
+
+alert(fruits.length); // 4
 //import { slider } from "./modules/_slider.js";
 //import { modal} from "./modules/modal.js";
