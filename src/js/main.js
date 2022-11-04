626b9bbe7a258c0f1f1578014c61cb26d37ae57b
Gelbrih <dreamlikeforest@gmail.com>
2022-11-04 13:26:43 +0300

    main js delete all and add alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,4 @@
 'use strict'
-let fruits = ["Яблоки", "Груша", "Апельсин"];
-
-let shoppingCart = fruits;
-
-shoppingCart.push("Банан");
-
-alert(fruits.length); // 4
-
-
-function getMaxSubSum(arr) {
-   let maxSum = 0;
-   let partialSum = 0;
-
-   for (let item of arr) { // для каждого элемента массива
-      partialSum += item; // добавляем значение элемента к partialSum
-      maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
-      if (partialSum < 0) partialSum = 0; // ноль если отрицательное
-   }
-
-   return maxSum;
-}
+alert(1);
 //import { slider } from "./modules/_slider.js";
 //import { modal} from "./modules/modal.js";
