cbe45a2b8fe1d206c46a8c37cb9e819a550a91a6
Gelbrih <dreamlikeforest@gmail.com>
2022-11-04 13:19:41 +0300

    add random function

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,19 @@ let shoppingCart = fruits;
 shoppingCart.push("Банан");
 
 alert(fruits.length); // 4
+
+
+function getMaxSubSum(arr) {
+   let maxSum = 0;
+   let partialSum = 0;
+
+   for (let item of arr) { // для каждого элемента массива
+      partialSum += item; // добавляем значение элемента к partialSum
+      maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
+      if (partialSum < 0) partialSum = 0; // ноль если отрицательное
+   }
+
+   return maxSum;
+}
 //import { slider } from "./modules/_slider.js";
 //import { modal} from "./modules/modal.js";
